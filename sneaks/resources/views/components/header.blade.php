7380f196bc2f68622a7933065d8b22ecf557609d
ddylvianen <87158436+ddylvianen@users.noreply.github.com>
2025-10-13 09:38:31 +0200

    feat: Add Bootstrap Icons dependency and integrate icon in header component

--- sneaks/resources/views/components/header.blade.php
+++ sneaks/resources/views/components/header.blade.php
@@ -18,6 +18,9 @@
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
+            <div>
+                <i class="bi bi-bag"></i>
+            </div>
         </div>
 
         @if (Auth::check())
